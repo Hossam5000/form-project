ace5a3769a49cc1ac7e212900be571fd4cf3ed0f
Hossam <codingWorld5000@gmail.com>
2025-12-17 15:04:19 +0200

    starting to save the ingred names and quants

--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,45 @@
 // cons & vars
 const container = document.querySelector(".container");
+const ingreds = document.querySelector(".ingreds");
 const createBtn = document.querySelector(".createBtn");
 
 console.log(createBtn)
 
 // functions
-function print() {
-    console.log(this.getAttribute("class"));
-    const ingredField = this.parentElement.querySelector(".ingred");
-    const clonedField = ingredField.cloneNode(true);
-    clonedField.querySelector(".ingredName").value = "";
-    clonedField.querySelector(".ingredQuant").value = "";
-    ingredField.parentElement.append(clonedField)
-    console.log(ingredField, clonedField);
+function addIngredField() {
+    /*
+    <div class="ingred">
+                    <input type="text" class="ingredName" placeholder="enter component">
+                    <input type="number" class="ingredQuant" placeholder="enter quantity" min="0.1">
+                </div><!--./ingred-->
+    */
+    //    creating the ingred div
+    const ingred = document.createElement("div");
+    ingred.classList.add("ingred");
+
+    // creating the name input field
+    const ingredName = document.createElement("input");
+    ingredName.type = "text";
+    ingredName.classList.add("ingredName");
+    ingredName.placeholder = "enter component";
+
+    // creating the quantity input field
+    const ingredQuant = document.createElement("input");
+    ingredQuant.type = "number";
+    ingredQuant.min = "0";
+    ingredQuant.step = "0.01";
+
+    ingredQuant.classList.add("ingredQuant");
+    ingredQuant.placeholder = "enter quantity";
+
+
+    //appending to the ingred
+    ingred.append(ingredName, ingredQuant);
+
+    // appending to the ingreds
+    ingreds.append(ingred);
 };
 
 // events
-createBtn.addEventListener("click", print);
+createBtn.addEventListener("click", addIngredField);
 // logic
